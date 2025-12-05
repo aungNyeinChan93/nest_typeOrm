1d8e12ae165443f1bcccbc38ed5a79e391581774
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-05 21:17:28 +0630

    add zod validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { type CreateUser } from './schemas/users.zodSchema';
+import { UserSchema, type CreateUser } from './schemas/users.zodSchema';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @UsePipes(new ZodValidationPipe(UserSchema))
   create(@Body() createUserDto: CreateUser) {
     return this.usersService.create(createUserDto);
   }
